74b46bea8c298062db4ca28463f1a920e5c16689
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-07-07 10:01:47 +0300

    release 2

--- Module_5_1.py
+++ Module_5_1.py
@@ -8,14 +8,10 @@ class House:
             for i in range(1, new_floor + 1):
                 print(i)
         else:
-            print(f'Этажа {new_floor} не существует')
-    def __str__(self):
-        return f'Имя дома:{self.name}, Этажей:{self.number_of_floors}'
+            print('Такого этажа не существует')
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-print(str(h1))
 h1.go_to(5)
-print(str(h2))
 h2.go_to(10)
